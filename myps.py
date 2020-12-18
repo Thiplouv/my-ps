e2e19da73f76fa238beb2f3ad4c62ce396e2abce
Thibault Plouviez <thibault.plouviez@icloud.com>
2020-12-18 18:50:32 +0100

    Add the myps.py file, with code for Level 1

---
+++ myps.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3
+
+import sys
+
+try :
+    if len(sys.argv) <= 2 :
+        print("{:>5} {:<8} {:>8} {:<27}".format("PID","TTY","TIME","CMD"))
+    if "-o" in sys.argv :
+        args = str(sys.argv[2]).upper()
+        args = args.split(",")
+
+        for word in args :
+            if "PID" in word or "PPID" in word or "CMD" in word :
+                print("{:>10} ".format(word))
+            else :
+                print("This program only supports \"PID\", \"PPID\" and \"CMD\" arguments for the moment")
+
+        #if "PID" in args and "PPID" in args and "CMD" in args :
+        #    print("{:>5} {:5>} {:<27}".format(args[0], args[1], args[2]))
+        #else :
+            #print("This program only supports \"PID\", \"PPID\" and \"CMD\" arguments for the moment")
+
+except ValueError :
+    print("Usage: ./myps.py options flag [arguments]")
