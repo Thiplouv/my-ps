194b012fbf8e08c4ef306c5adecc70b31a3caa8b
Thibault Plouviez <thibault.plouviez@icloud.com>
2020-12-28 19:40:00 +0100

    Add better bad keyword(s) recognition

--- myps.py
+++ myps.py
@@ -19,12 +19,21 @@ def generate_template(column_name):
         if settings[i][0] == column_name :
             return(settings[i][1])
 
+def verif_keywords(keywords) :
+    badkwd = []
+    for word in keywords :
+        if all(word != settings[i][0] for i in range(len(settings))) is True :
+            badkwd.append(word)
+    return badkwd
+
 try :
     if len(sys.argv) <= 2 :
         print("{:>5} {:<8} {:>8} {:<27}".format("PID","TTY","TIME","CMD"))
     if "-o" in sys.argv :
-        if "PID" not in args and "PPID" not in args and "CMD" not in args and "COMMAND" not in args and "TTY" not in args and "TIME" not in args :
-            print("ps: {:s}: keyword not found".format(sys.argv[2]))
+        verif = verif_keywords(args)
+        if len(verif) != 0 :
+            for i in range(len(verif)) :
+                print("ps: {:s}: keyword not found".format(verif[i]))
             print("ps: no valid keywords; valid keywords:\npid ppid cmd command tty time")
         else :
             for word in args :
